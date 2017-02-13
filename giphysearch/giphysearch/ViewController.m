9426d5b179cf22cf05a47260bb1f7e4b11098c1e
Susheel Daswani <sdaswani@cs.stanford.edu>
2017-02-13 12:08:45 -0800

    Appearance Tweaks
    Get the appearance of the app a bit more up to spec. There are some
    hacks here but can try to make it better later.

--- giphysearch/giphysearch/ViewController.m
+++ giphysearch/giphysearch/ViewController.m
@@ -20,12 +20,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [AXCGiphy setGiphyAPIKey:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"GiphyAPIKey"]];
-    self.searchBar.placeholder = @"Search Giphy";
+    [self setNeedsStatusBarAppearanceUpdate];
+    
+    // UISearchBar Color Customizations - BEGIN
+    self.searchBar.backgroundImage = [[UIImage alloc] init];
+    self.searchBar.backgroundColor = [UIColor darkGrayColor];
+    // TODO: Fix this terrible hack
+    self.searchBar.placeholder = @"Search Giphy                                                                                                                        ";
+    
+    // Magnifying glass icon needs to change color and be repositioned
+    UITextField *searchBarTextField = [self.searchBar valueForKey:@"_searchField"];
+    //[searchBarTextField setLeftViewMode:UITextFieldViewModeNever];
+    [searchBarTextField setRightViewMode:UITextFieldViewModeNever];
+    UIImageView *leftImageView = (UIImageView *)searchBarTextField.leftView;
+    leftImageView.image = [leftImageView.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    leftImageView.tintColor = [UIColor greenColor];
+    // UISearchBar Color Customizations - END
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UI customizations
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
 }
 
 #pragma mark UISearchBarDelegate methods
